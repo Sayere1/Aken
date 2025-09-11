156ab6cdc9511041c4a2a1122cbd9fb806c216f9
Osayemwenre Osaigbovo <122838434+Sayere1@users.noreply.github.com>
2025-09-11 11:35:14 +0200

    simplifying coms between client and server apps using TypeScript(TRPC)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+import { TRPCReactProvider } from "@/trpc/client";
+
 const inter = Inter({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -20,6 +22,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+
+    <TRPCReactProvider>
     <html lang="en">
       <body
         className={`${inter.className} antialiased`}
@@ -27,5 +31,6 @@ export default function RootLayout({
         {children}
       </body>
     </html>
+    </TRPCReactProvider>
   );
 }
--- src/modules/home/ui/views/home-view.tsx
+++ src/modules/home/ui/views/home-view.tsx
@@ -1,6 +1,32 @@
 "use client"
 
-import { Button } from "@/components/ui/button";
+import { useTRPC } from "@/trpc/client";
+import { useQuery } from "@tanstack/react-query";
+
+export const HomeView = () => {
+
+    const trpc = useTRPC();
+    const { data } = useQuery(trpc.hello.queryOptions({text: "Osaze"}));
+
+    return (
+        <div className="flex flex-col p-4 gap-y-4">
+            {data?.greeting}
+        </div>
+    );
+};
+
+
+
+
+
+
+
+
+
+
+
+
+/*import { Button } from "@/components/ui/button";
 import { authClient } from "@/lib/auth-client";
 import { useRouter } from "next/navigation";
 
@@ -21,14 +47,15 @@ export const HomeView = () => {
         <div className="flex flex-col p-4 gap-y-4">
             <p>Logged in as {session.user.name} </p>
             <Button onClick={() => authClient.signOut({
-                fetchOptions: { 
+                fetchOptions: {
                     onSuccess: () => router.push("/sign-in")
-                    }
-                    })
-                    }>
+                }
+            })
+            }>
                 Sign out
             </Button>
 
         </div>
     )
 };
+*/
