e30b157385c1032e44308ba4e5b18672211a1942
Osayemwenre Osaigbovo <122838434+Sayere1@users.noreply.github.com>
2025-09-17 11:57:09 +0200

    meetings ui created and implemented with the db

--- src/modules/meetings/ui/views/meetings-view.tsx
+++ src/modules/meetings/ui/views/meetings-view.tsx
@@ -1,17 +1,26 @@
 "use client"
 
+import { DataTable } from "@/components/data-table";
 import { ErrorState } from "@/components/error-state";
 import { LoadingState } from "@/components/loading-state";
 import { useTRPC } from "@/trpc/client"
 import { useSuspenseQuery } from "@tanstack/react-query";
+import { columns } from "../components/columns";
+import { EmptyState } from "@/components/empty-state";
+
 
 export const MeetingsView = () => {
     const trpc = useTRPC();
     const { data } = useSuspenseQuery(trpc.meetings.getMany.queryOptions({}))
 
     return (
-        <div className="overflow-x-scroll">
-          {/*JSON.stringify(data)*/}
+        <div className="flex flex-1 pb-4 flex-col px-4 gap-y-4 md:px-8">
+          <DataTable data={data.items} columns={columns} />
+          {data.items.length === 0 && (
+                          <EmptyState title="create your first meeting"
+                          description="Create a meeting to connect with others, each meeting let you 
+                          collaborate, share ideas and interact with participant in real time" />
+                      )}
         </div>
     )
 }
